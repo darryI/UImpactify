870664a416e7a34a2ca03e98ef5592222c1fca2d
ObaidaSaleh <46760451+ObaidaSaleh@users.noreply.github.com>
2020-10-31 11:10:52 -0400

    LUK-75 added slashes to the end of all routes, routes with variable at the end still work

--- backend/uimpactify/controller/routes.py
+++ backend/uimpactify/controller/routes.py
@@ -28,10 +28,10 @@ def create_routes(api: Api):
     api.add_resource(SelfDeleteApi, '/user/delete-self/')
 
     api.add_resource(CoursesApi, '/course/')
-    api.add_resource(CourseApi, '/course/<course_id>')
+    api.add_resource(CourseApi, '/course/<course_id>/')
 
     api.add_resource(CourseByInstructorApi, '/course/instructor/')
 
-    api.add_resource(CourseEnrollmentApi, '/course/enroll')
-    api.add_resource(CourseDisenrollmentApi, '/course/disenroll/<course_id>/<user_id>')
-    api.add_resource(CoursesWithStudentApi, '/course/student/<student_id>')
+    api.add_resource(CourseEnrollmentApi, '/course/enroll/')
+    api.add_resource(CourseDisenrollmentApi, '/course/disenroll/<course_id>/<user_id>/')
+    api.add_resource(CoursesWithStudentApi, '/course/student/<student_id>/')
