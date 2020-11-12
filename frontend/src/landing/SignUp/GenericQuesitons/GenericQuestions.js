401b40867b4698fead5fe3c7d3dc0e95ea82775d
unknown <datguybatu@gmail.com>
2020-11-12 18:19:21 +0300

    LUK-69 added unit tests for signup inside generic questions

--- frontend/src/landing/SignUp/GenericQuesitons/GenericQuestions.js
+++ frontend/src/landing/SignUp/GenericQuesitons/GenericQuestions.js
@@ -6,20 +6,20 @@ const GenericQuestions = (props) => {
     return(
         <form className="SignUpGQ">
             <h2 className="SignUpSubText">Create an Account</h2>
-            <label className="SignUpInputLabel" htmlFor="email">Email:</label><br/>
-            <input className="SignUpInfoInput" type="text" name="email" placeholder="Email" 
+            <label className="SignUpInputLabel" htmlFor="Email">Email:</label><br/>
+            <input id="Email" className="SignUpInfoInput" type="text" name="email" placeholder="Email" 
             value={props.email} onChange={props.handleEmailChange}/>
 
             <label className="SignUpInputLabel" htmlFor="Name">Name:</label><br/>
-            <input className="SignUpInfoInput" type="text" name="Name" placeholder="Name" 
+            <input id="Name" className="SignUpInfoInput" type="text" name="Name" placeholder="Name" 
             value={props.username} onChange={props.handleUsernameChange}/>
 
-            <label className="SignUpInputLabel" htmlFor="password">Password:</label><br/>
-            <input className="SignUpInfoInput" type="password" name="password" placeholder="Password" 
+            <label className="SignUpInputLabel" htmlFor="Password">Password:</label><br/>
+            <input id="Password" className="SignUpInfoInput" type="password" name="password" placeholder="Password" 
             value={props.password} onChange={props.handlePasswordChange}/>
 
-            <label className="SignUpInputLabel" htmlFor="phone">Phone:</label><br/>
-            <input className="SignUpInfoInput" type="text" name="phone" placeholder="Phone number" 
+            <label className="SignUpInputLabel" htmlFor="Phone">Phone:</label><br/>
+            <input id="Phone" className="SignUpInfoInput" type="text" name="phone" placeholder="Phone number" 
             value={props.phone} onChange={props.handlePhoneChange}/>
         </form>
     )
