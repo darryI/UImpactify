60da2609b969f733aeae4033ddf72b5b2dce5c3c
Case Ploeg <caseploog@gmail.com>
2020-11-12 20:22:53 -0500

    LUK-86 multiple choice questions
    Creation form allows users to create new multi choice questions and edit existing ones.
    
    Default number of options for a question is 4. Answer is specified using the radio buttons found aside each option.
    
    This required a lot of nesting :)

--- frontend/src/courses/quizzes/QuizForm/QuizForm.js
+++ frontend/src/courses/quizzes/QuizForm/QuizForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useHistory} from "react-router-dom";
 
 import { ReactComponent as SaveIcon } from 'icons/save.svg';
-
+import { ReactComponent as AddIcon } from 'icons/add.svg';
 import './QuizForm.css';
 
 
@@ -10,6 +10,9 @@ function QuizForm(props) {
 
   const values = props.values;
   const history = useHistory();
+
+  
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -63,6 +66,98 @@ function QuizForm(props) {
     });
   }
 
+  const handleQuestionChange = (event, i) => {
+    // unpack the quiz questions, and updated the current question's question (text) field
+    const updatedQuestions = [...values.quizQuestions]
+    updatedQuestions[i].question = event.target.value;
+    
+    props.setValues({
+      ...values,
+      quizQuestions: updatedQuestions
+    });
+  }
+
+  const handleOptionChange = (event, i, j) => {
+    const updatedQuestions = [...values.quizQuestions]
+    updatedQuestions[i].options[j].option = event.target.value;
+
+    props.setValues({
+      ...values,
+      quizQuestions: updatedQuestions
+    });
+  }
+
+  const handleAnswerChange = (event, i, j) => {
+    const updatedQuestions = [...values.quizQuestions]
+    updatedQuestions[i].answer = j;
+
+    props.setValues({
+      ...values,
+      quizQuestions: updatedQuestions
+    });
+  }
+  
+  const handleNewQuestion = (event) => {
+    // preventdefault to stop form from prematurely submitting
+    event.preventDefault();
+    const updatedQuestions = [...values.quizQuestions];
+    const newQuestionValues = {
+      "question": "to be or not to be?",
+      "answer": 0,
+      "options": [
+        {
+          index: 0,
+          option: ""
+        },
+        {
+          index: 1,
+          option: ""
+        },
+        {
+          index: 2,
+          option: ""
+        },
+        {
+          index: 3,
+          option: ""
+        }
+      ],
+      "index": values.quizQuestions.length
+    }
+  
+    updatedQuestions.push(newQuestionValues);
+    props.setValues({
+      ...values,
+      quizQuestions: updatedQuestions
+    });
+  };
+
+  // create input element for the multiple choice questions
+  const questions = values.quizQuestions.map((qu, i) => {
+
+    const options = [0, 1, 2, 3].map((j) => {
+      return (
+        <div>
+          <label className="option-text"></label>
+          <input aria-label="option-text" className="rect-1643 mult-option" type="text" value={values.quizQuestions[i].options[j].option} onChange={e => handleOptionChange(e, i, j)} />
+          <input class="radio-answer"  title="Correct Answer" type="radio" name={`answer-${i}`} checked={values.quizQuestions[i].answer === j} onChange={e => handleAnswerChange(e, i, j)}></input>
+        </div>
+      )
+    });
+
+    return (
+      <div className="mult-question">
+        
+        <label className="question-text"><h2>Question {i+1}</h2></label>
+        <input aria-label="question-text" className="rect-1643" type="text" value={values.quizQuestions[i].question} onChange={e => handleQuestionChange(e, i)} />
+
+        <h2>Options</h2>
+        {options}
+      </div>
+    )
+  });
+
+
   return (
     <form aria-label="creation-form" className="creationForm" onSubmit={handleSubmit}>
       <div className="labelRectCombo">
@@ -70,7 +165,13 @@ function QuizForm(props) {
         <input aria-label="name-input" className="rect-1643"   type="text" value={values.name} onChange={e => handleNameChange(e)} />
       </div>
 
-      
+      <h1>Multiple Choice Questions</h1>
+      {questions}
+      <button onClick={e => handleNewQuestion(e)} className="create-button">
+        <AddIcon width="30px" height="30px"/>
+        New Question
+      </button>
+
       <div className="row">
         <div className="labelRectCombo">
           <label className="label-text">Publish?</label>
