59b0ce84ceef23b35c7d099d908ca7bee141f79c
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2020-11-16 15:18:55 +0800

    [LUK-55] Adding unit test for Student dashboard page

---
+++ frontend/src/landing/StudentDashboard/StudentDashboard.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, wait } from '@testing-library/react';
+
+import StudentDashboard, {API} from './StudentDashboard.js';
+import jsonCourses from 'courses/courses.json';
+import { StaticRouter } from 'react-router-dom'
+
+const setup = () => {
+  const utils = render(
+    // need to wrap the StudentDashboard component with a static router to 
+    // simulate the Router information for the real app
+    // This is needed because we use <Link/> tags inside <StudentDashboard /> 
+    //                                            in <DashboardCrouseCard> in <LearnMoreButton />
+
+    // ref: https://reactrouter.com/web/guides/testing
+    <StaticRouter>
+        <StudentDashboard />
+    </StaticRouter>
+    
+  );
+    
+  return {
+    ...utils,
+  }
+};
+
+test('on startup, api call is made to get courses with this signed-in student', async () => {
+  const getFunc = jest.spyOn(API, 'getCourses').mockImplementationOnce(() => {
+    return Promise.resolve(jsonCourses);
+  })
+  
+  const { container } = setup();
+
+  await wait (() => expect(getFunc).toHaveBeenCalledTimes(1));
+
+  // check that all the cards in the test data are rendered
+  const dashboardCourseCards = container.querySelectorAll('.dashboard-course-card');
+  expect(dashboardCourseCards.length).toBe(jsonCourses.length);
+});
