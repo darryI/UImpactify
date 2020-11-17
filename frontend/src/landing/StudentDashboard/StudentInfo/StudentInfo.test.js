b8a0305b0a225a99f30428a91ae4c37fc14ae9fc
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2020-11-18 01:57:26 +0800

    [LUK-93] Adding unit test for StudentInfo displaying

--- frontend/src/landing/StudentDashboard/StudentInfo/StudentInfo.test.js
+++ frontend/src/landing/StudentDashboard/StudentInfo/StudentInfo.test.js
@@ -1,42 +1,41 @@
-// import React from 'react';
-// import { render, fireEvent, wait } from '@testing-library/react';
-// import StudentInfo, {API} from './StudentInfo.js';
+import React from 'react';
+import { render, wait } from '@testing-library/react';
+import StudentInfo, {API} from './StudentInfo.js';
 
-// const setup = () => {
-//     // const accessToken = 'fake-token';
-//     // const setAccessToken = jest.fn((values) => {});
+const setup = () => {
+    jest.spyOn(window.localStorage.__proto__, 'getItem').mockImplementation((key) => '{"accessToken":"yo"}');
 
-//     const utils = render(
-//         <StudentInfo />
-//     )
+    const utils = render(
+        <StudentInfo />
+    )
 
-//     return {
-//         ...utils,
-//     }
-// };
+    return {
+        ...utils,
+    }
+};
 
-// test('on startup, api call is made to get published courses', async () => {
-//     // moock user
-//     const mockUser = {
-//         "name": "ymart1n",
-//         "email": "1231293@ww.com",
-//         "phone": "1234567980"
-//     };
+test('on startup, api call is made to get published courses', async () => {
+    // moock user
+    const mockUser = {
+        "name": "ymart1n",
+        "email": "1231293@ww.com",
+        "phone": "1234567980"
+    };
 
-//     const getFunc = jest.spyOn(API, 'getUser').mockImplementationOnce(() => {
-//         return Promise.resolve(mockUser);
-//     })
+    const getFunc = jest.spyOn(API, 'getUser').mockImplementationOnce(() => {
+        return Promise.resolve(mockUser);
+    })
+    
+    const { getByText } = setup();
 
-//     const { getByText } = setup();
+    await wait (() => expect(getFunc).toHaveBeenCalled());
 
-//     await wait (() => expect(getFunc).toHaveBeenCalled());
-
-//     const welcome = getByText("Welcome back!  " + mockUser.name);
-//     expect(welcome).toBeInTheDocument();
-//     const email = getByText("Email: " + mockUser.email);
-//     expect(email).toBeInTheDocument();
-//     const phone = getByText("Phone: " + mockUser.phone);
-//     expect(phone).toBeInTheDocument();
+    const welcome = getByText(mockUser.name);
+    expect(welcome).toBeInTheDocument();
+    const email = getByText(mockUser.email);
+    expect(email).toBeInTheDocument();
+    const phone = getByText(mockUser.phone);
+    expect(phone).toBeInTheDocument();
 
 
-// });
+});
