3915fcc6a5ca04142dd297784add105861b21c7c
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2020-11-29 20:06:08 +0800

    [LUK-57] Improved top bar so that it shows multiple dashboard links if the user has multiple roles

--- frontend/src/utils/Navigation.js
+++ frontend/src/utils/Navigation.js
@@ -5,116 +5,44 @@ function TopBar(props) {
 
     const loggedIn = props.loggedIn;
     const user = props.user;
-
+    
     if (loggedIn && user.roles){
-      if (user.roles.student) {
-        return (
-          <div aria-label="topBar">
-            <Link to="/logout">
-              <button className="navbarButton">LOGOUT</button>
-            </Link>
-            <img id="logo" src={require('../images/logo.svg')} alt="Logo"/>
-            <nav className="navbar">
-              <ul>
-                <li className="navItem">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/dashboard">Student</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/courses">Courses</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        );
+      const isStudent = user.roles.student;
+      const isInstructor = user.roles.instructor;
+      const isNPO = user.roles.organization;
+      const isAdmin = user.roles.admin;
 
-      } else if (user.roles.instructor) {
-        return (
-          <div aria-label="topBar">
-            <Link to="/logout">
-              <button className="navbarButton">LOGOUT</button>
-            </Link>
-            <img id="logo" src={require('../images/logo.svg')} alt="Logo"/>
-            <nav className="navbar">
-              <ul>
-                <li className="navItem">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/create">Instructor</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/courses">Courses</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        );
-      } else if (user.roles.organization) {
-        return (
-          <div aria-label="topBar">
-            <Link to="/logout">
-              <button className="navbarButton">LOGOUT</button>
-            </Link>
-            <img id="logo" src={require('../images/logo.svg')} alt="Logo"/>
-            <nav className="navbar">
-              <ul>
-                <li className="navItem">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/jobs">Social Initiative</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/courses">Courses</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        );
-      } else {
-        return (
-          <div aria-label="topBar">
-            <Link to="/logout">
-              <button className="navbarButton">LOGOUT</button>
-            </Link>
-            <img id="logo" src={require('../images/logo.svg')} alt="Logo"/>
-            <nav className="navbar">
-              <ul>
-                <li className="navItem">
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/about">About</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/create">Instructor</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/jobs">Social Initiative</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/courses">Courses</Link>
-                </li>
-                <li className="navItem">
-                  <Link to="/dashboard">Student</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        );
-      }
+      return (
+        <div aria-label="topBar">
+          <Link to="/logout">
+            <button className="navbarButton">LOGOUT</button>
+          </Link>
+          <img id="logo" src={require('../images/logo.svg')} alt="Logo"/>
+          <nav className="navbar">
+            <ul>
+              <li className="navItem">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="navItem">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="navItem" style={{ display: isStudent || isAdmin ? "inline-block" : "none" }}>
+                <Link to="/dashboard">Student</Link>
+              </li>
+              <li className="navItem" style={{ display: isInstructor || isAdmin ? "inline-block" : "none" }}>
+                <Link to="/create">Instructor</Link>
+              </li>
+              <li className="navItem" style={{ display: isNPO || isAdmin ? "inline-block" : "none" }}>
+                <Link to="/jobs">Social Initiative</Link>
+              </li>
+              <li className="navItem">
+                <Link to="/courses">Courses</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      );
+      
     } else {
         return (
           <div aria-label="topBar">
